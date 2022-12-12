c331d97eb4ec8df47f2351983e0b5ad91f6b66ba
Tyler <ty1erk@hotmail.com>
2022-12-12 17:30:48 +0000

    fixed login failure route

--- login.py
+++ login.py
@@ -13,19 +13,26 @@ def signInLanding():
     return render_template("login.html")
 
 
-@login.route("/success", methods = ['POST'])
-def success():
-    response = requests.post('http://localhost:8080/api/v1/login', json={
+@login.route("/session", methods = ['POST'])
+def signin():
+    response = requests.post('http://localhost:8080/api/v1/users/login', json={
         "email": request.form["email"],
         "password": request.form["password"] 
     })
-     
-    response_header= response.json()
-    print(response_header)
-    token = response_header["token"]
-    res = make_response(render_template("userProfile.html"))
-    res.set_cookie("token",token)
-    print(token)
 
-    return res, token
+    if response.status_code == 200:
+     
+        response_header= response.json()
+        print(response_header)
+        token = response_header["token"]
+        res = make_response(render_template("userProfile.html"))
+        res.set_cookie("token",token)
+        print(token)
+        return res, token
+
+    elif response.status_code == 401:
+        res = make_response(render_template("incorrect.html"))
+        return res
+
+
     
--- register.py
+++ register.py
@@ -8,35 +8,11 @@ def registering():
     #allows for passing of html to Python, can also pass variables INTO the html
     return render_template("register.html")
 
-#COOOOOKIEEEEEE
-@register.route("/registering")
-def cookies():
-    #Anything I would pass into return, can be passed into make_response
-    res = make_response("Checking Cookie", 200)
-    
 
-    
-    res.set_cookie(
-                    "token", 
-                   value="eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIxMjM3MzA3NjY0ODcwOTMyNDgifQ.AN",
-                   #expiry Age in seconds
-                   #max_age=10,
-                   #takes date/time object
-                   expires = None,
-                   #can specify the path
-                   path="/",
-                   #domain that can read cookie, can work with subdomains etc
-                   domain=None,
-                   #setting to true only allows HTTPS
-                   secure=False,
-                   #sets that cookie if cookie is only accessible via JS or HTTP
-                   httponly=False,
-                   #Limits scope of accessibility
-                   #samesite=False
-                   )
-    
-    res.set_cookie("Chocolate type", "dark")
-    
-    res.set_cookie("chewy", "yes")
-    
-    return res
+@register.route("/createuser", methods = ['GET', 'POST'])
+def signup():
+    data = request.json()
+
+    new_user = User(
+        email=data.get('email')
+    )
