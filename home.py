0dc5790c463bf538f21eb9ef20d19ae523b9e124
Tyler <ty1erk@hotmail.com>
2022-12-19 11:43:51 +0000

    stuff

--- home.py
+++ home.py
@@ -14,8 +14,6 @@ home = Blueprint(__name__, "home")
 def getItems():
     response = requests.get('http://localhost:8080/api/v1/items')
     # print (response.json())
-    categoriesRes = requests.get('http://localhost:8080/api/v1/categories')
-    categories = categoriesRes.json()
     items = response.json()
     pages = math.ceil(len(items)/24)
     args = request.args
@@ -30,7 +28,7 @@ def getItems():
         length = int(args.get('length'))
 
     svg = './static/assets/basket.svg'
-    res = render_template('index.html', items=items, pages=pages, start=start, end=end, length=length, svg=svg, categories=categories)
+    res = render_template('index.html', items=items, pages=pages, start=start, end=end, length=length, svg=svg)
     return res
 
 # @home.route("/getitem")
