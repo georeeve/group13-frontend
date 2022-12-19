f2392b38647ff51ebdc4431116f97b7a22919c3d
Tyler <ty1erk@hotmail.com>
2022-12-19 12:43:11 +0000

    trial and error on filtering

--- home.py
+++ home.py
@@ -15,14 +15,20 @@ def getItems():
     response = requests.get('http://localhost:8080/api/v1/items')
     #print (response.json())
     categoriesRes = requests.get('http://localhost:8080/api/v1/categories')
+
     categories = categoriesRes.json()
     print(categories)
+
     items = response.json()
+
     pages = math.ceil(len(items)/24)
+
     args = request.args
+
     start = 0
     end = 24
     length = len(items)
+
     if args.get('start') is not None:
         start = int(args.get('start'))
     if args.get('end') is not None:
@@ -30,7 +36,11 @@ def getItems():
     if args.get('length') is not None:
         length = int(args.get('length'))
 
+    if args.get('category') is not None:
+        items = list(filter(lambda item: item['category']['id'] == int(args.get('category')), items))
+
     svg = './static/assets/basket.svg'
+
     res = render_template('index.html', items=items, pages=pages, start=start, end=end, length=length, svg=svg, categories=categories)
     return res
 
