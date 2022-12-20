9556e793d064deec92afa29e62864f6eff279316
georeeve <116298340+georeeve@users.noreply.github.com>
2022-12-20 16:02:21 +0000

    Store basket info (#17)

--- home.py
+++ home.py
@@ -1,15 +1,16 @@
-#Setting up routes
-from flask import Blueprint, render_template, request
+# Setting up routes
+import base64
+import json
+
+from flask import Blueprint, render_template, request, make_response
 import requests
 import math
-#request allows us to do profile?name= in browser
+# request allows us to do profile?name= in browser
 
-#initialise Blueprint
+# initialise Blueprint
 home = Blueprint(__name__, "home")
 
-#creating / route
 
-# connect to api
 @home.route("/", methods = ['GET'])
 def getItems():
     response = requests.get('http://localhost:8080/api/v1/items')
@@ -33,10 +34,10 @@ def getItems():
         end = int(args.get('end'))
     if args.get('length') is not None:
         length = int(args.get('length'))
-    
+
     if args.get('category') is not None:
         print(items)
- 
+
         items = list(filter(lambda item: item['category']['id'] == int(args.get('category')), items))
         print(items)
 
@@ -44,8 +45,19 @@ def getItems():
     return res
 
 
+@home.route("/basketadd", methods=['POST'])
+def add_item():
+    data = request.get_json()
+    basket_cookie = request.cookies.get("basket")
+    basket = json.loads(base64.b64decode(basket_cookie.encode('ascii')).decode('ascii')) if basket_cookie is not None else {}
+    item_id = data['itemId']
+    current_quantity = basket[item_id]['quantity'] if basket.get("item_id") is not None else 0
+    to_add_quantity = int(data['quantity'])
+    basket[item_id] = current_quantity + to_add_quantity
+    res = make_response()
+    res.set_cookie("basket", base64.b64encode(json.dumps(basket).encode('ascii')).decode('ascii'))
+    return res
 
-    
 
 if __name__ == '__main__':
     home.run()
