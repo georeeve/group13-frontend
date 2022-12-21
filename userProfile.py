45f7d93caec56b52e586cf993d0550be69101f9a
Ana Moros <ana.morosmarmol@sky.uk>
2022-12-21 17:27:32 +0000

    patch request

--- userProfile.py
+++ userProfile.py
@@ -1,11 +1,47 @@
 from flask import Blueprint, render_template, request, url_for, make_response
 
 import requests
+import json
 
 userprofile = Blueprint(__name__, "userprofile")
 
-@userprofile.route('/', methods = ['GET', 'PATCH'])
+@userprofile.route('/', methods = ['GET', 'POST'])
 def userProfile():
+    userRes = load_userData()
+    # print(response_admin)
+    
+
+    # if response_admin["admin"] == True:
+
+    #         usersResponse = requests.get("http://localhost:8080/api/v1/users", headers={"Authorization": "Bearer " + token})
+    #         users = usersResponse.json()
+    #         print(users)
+    #         adminRes = make_response(render_template('userProfileAdmin.html', email=email, name=name, lastname=lastname, address=address, addressTwo=addressTwo, city=city, postCode=postCode ))
+
+    #         return adminRes
+
+    # else:
+    if request.method == 'GET':
+        return userRes
+
+    elif request.method == 'POST':
+        form_data = request.form
+        
+        data = request.form.to_dict()
+
+        for key, val in form_data.items():
+            if val == '':
+                del data[key]
+        
+        token = request.cookies.get('token')
+        update = requests.patch('http://localhost:8080/api/v1/user', json=data, headers={"Authorization": "Bearer " + token})
+            
+        userRes = load_userData()
+        return userRes
+
+        
+        
+def load_userData():
     token = request.cookies.get('token')
     response = requests.get("http://localhost:8080/api/v1/user", headers={"Authorization": "Bearer " + token})
     
@@ -18,40 +54,5 @@ def userProfile():
     city = response.json()["city"]
     postCode = response.json()["postCode"]
 
-    print(response_admin)
-    
-
-    if response_admin["admin"] == True:
-
-            usersResponse = requests.get("http://localhost:8080/api/v1/users", headers={"Authorization": "Bearer " + token})
-            users = usersResponse.json()
-            print(users)
-            adminRes = make_response(render_template('userProfileAdmin.html', email=email, name=name, lastname=lastname, address=address, addressTwo=addressTwo, city=city, postCode=postCode ))
-
-            return adminRes
-
-    else:
-        if request.method == 'GET':
-            userRes = make_response(render_template('userProfile.html',email=email,name=name, lastname=lastname, address=address, addressTwo=addressTwo, city=city, postCode=postCode))
-            return userRes
-
-        elif request.method == 'PATCH':
-            update = requests.patch('http://localhost:8080/api/v1/user', json={
-            "email": request.form["email"],
-            "password": request.form["password"],
-            "firstName": request.form["firstName"],
-            "lastName": request.form["lastName"],
-            "dob": request.form["dob"],
-            "addressLine1": request.form["addressLine1"],
-            "addressLine2": request.form["addressLine2"],
-            "city": request.form["city"],
-            "postCode": request.form["postCode"]
-            })
-            
-            userUpdate = update.json()
-
-            return update
-        
-       
-
-
+    userRes = make_response(render_template('userProfile.html',email=email,name=name, lastname=lastname, address=address, addressTwo=addressTwo, city=city, postCode=postCode))
+    return userRes
