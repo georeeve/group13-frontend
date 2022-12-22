cc1e147e5d82ff47cbd6d8dd37b2c63912034c70
Ana Moros <ana.morosmarmol@sky.uk>
2022-12-22 10:26:56 +0000

    made dry code

--- userProfile.py
+++ userProfile.py
@@ -8,20 +8,8 @@ userprofile = Blueprint(__name__, "userprofile")
 
 @userprofile.route('/', methods=['GET', 'POST'])
 def userProfile():
-    userDict, admin = load_userData()
-    print(userDict, admin)
-
-    # allUsers = load_AllUsers()
-    # print(type(allUsers))
-
-    if request.method == 'GET':
-        template = 'userProfile.html' if not admin else 'userProfileAdmin.html'
-        userRes = make_response(render_template(template, user=userDict))
-        return userRes
-
-    elif request.method == 'POST':
+    if request.method == 'POST':
         form_data = request.form
-
         data = request.form.to_dict()
 
         for key, val in form_data.items():
@@ -29,14 +17,13 @@ def userProfile():
                 del data[key]
 
         token = request.cookies.get('token')
-        update = requests.patch('http://localhost:8080/api/v1/user',
-                                json=data, headers={"Authorization": "Bearer " + token})
+        requests.patch('http://localhost:8080/api/v1/user',
+                       json=data, headers={"Authorization": "Bearer " + token})
 
-        userDict = load_userData()
-
-        template = 'userProfile.html' if not admin else 'userProfileAdmin.html'
-        userRes = make_response(render_template(template, user=userDict))
-        return userRes
+    userDict = load_userData()
+    template = 'userProfile.html' if not userDict["admin"] else 'userProfileAdmin.html'
+    userRes = make_response(render_template(template, user=userDict))
+    return userRes
 
 
 def load_userData():
@@ -45,7 +32,7 @@ def load_userData():
                             headers={"Authorization": "Bearer " + token})
 
     userDict = response.json()
-    return userDict, userDict["admin"]
+    return userDict
 
 
 def load_AllUsers():
