af636051f0b7b29fff48dcf5acd85313433badb8
Tyler <ty1erk@hotmail.com>
2022-12-22 14:47:31 +0000

    fixed Dob on userprofile/admin profile

--- static/dob.js
+++ static/dob.js
@@ -3,4 +3,8 @@ const today = new Date();
 const maxDate = new Date(today.getFullYear() - 18, today.getMonth(),
 today.getDate());
 
-document.getElementById("dob").max = maxDate.toISOString().split("T")[0]
+// document.getElementById("dob").max = maxDate.toISOString().split("T")[0] 
+
+const minAge = [...document.getElementsByClassName("dob")]
+
+minAge.forEach(element => element.max = maxDate.toISOString().split("T")[0] )
