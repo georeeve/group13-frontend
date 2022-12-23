696b041b6bacfc4c7fa210afca47eeb0a6f5e2a0
George Reeve <george.fr.reeve@gmail.com>
2022-12-23 11:11:11 +0000

    Reformat

--- src/routes/reset_password.py
+++ src/routes/reset_password.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template, flash, redirect, url_for
 reset_password = Blueprint("reset_password", "reset_password")
 
 
-@reset_password.route("/", methods=['GET'])
+@reset_password.route("/", methods=["GET"])
 def reset_password_get():
     return render_template("reset-password.html")
 
 
-@reset_password.route("/", methods=['POST'])
+@reset_password.route("/", methods=["POST"])
 def reset_password_post():
     flash("Sent reset request", "info")
-    return redirect(url_for('home.items_get'))
+    return redirect(url_for("home.items_get"))
